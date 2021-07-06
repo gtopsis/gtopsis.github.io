91b14799f8812eceee38318e3c6c444ec2b43256
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-07-06 12:40:12 +0300

    feat: support firm's logo in timeline

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -2,48 +2,75 @@
   <v-container fluid>
     <v-row align="center" class="justify-center justify-sm-left">
       <v-col cols="auto">
-        <h3>Experience</h3>
+        <h2>Experience</h2>
       </v-col>
     </v-row>
-    <v-timeline>
-      <v-timeline-item v-for="(element, index) in timelineItems" :key="index">
-        <template #icon>
-          <v-avatar :color="element.avatar.bgColor">
-            <a
-              :href="element.avatar.label.link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img :src="element.avatar.img" />
-            </a>
-          </v-avatar>
-        </template>
-        <template #opposite>
-          <a
-            :href="element.avatar.label.link"
-            class="firm-link"
-            target="_blank"
-            rel="noopener noreferrer"
+
+    <v-row align="center" class="justify-center">
+      <v-col cols="12" md="8" lg="6" class="px-0">
+        <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+          <v-timeline-item
+            v-for="(element, index) in timelineItems"
+            :key="index"
           >
-            <span>{{ element.avatar.label.text }}</span>
-          </a>
-        </template>
-        <v-card class="elevation-2">
-          <v-card-title class="text-h6">
-            {{ element.card.role }}
-          </v-card-title>
-          <v-card-subtitle>
-            {{ element.card.period.start }} -
-            {{ element.card.period.end }}
-          </v-card-subtitle>
-          <v-card-text>{{ element.card.description }}</v-card-text>
-          <v-card-actions>
-            <!-- <nuxt-link :to="element.card.action">Read more</nuxt-link> -->
-            Read more
-          </v-card-actions>
-        </v-card>
-      </v-timeline-item>
-    </v-timeline>
+            <template #icon>
+              <v-avatar
+                size="32"
+                :color="element.avatar.bgColor"
+                align-center
+                justify-center
+              >
+                <v-img
+                  contain
+                  :src="element.avatar.img"
+                  :alt="element.avatar.alt"
+                  class="firm-logo"
+                  width="24"
+                  height="24"
+                  @click="navigateToCompany(element.avatar.label.link)"
+                >
+                </v-img>
+              </v-avatar>
+            </template>
+            <template #opposite>
+              <a
+                :href="element.avatar.label.link"
+                class="firm-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>{{ element.avatar.label.text }}</span>
+              </a>
+            </template>
+            <v-card class="elevation-2 timeline-item">
+              <v-card-title class="text-h6">
+                {{ element.card.role }}
+              </v-card-title>
+              <v-card-subtitle>
+                {{ element.card.period.start }} -
+                {{ element.card.period.end }}
+              </v-card-subtitle>
+              <v-card-text
+                v-if="
+                  element.card.description || element.card.tools.length != 0
+                "
+              >
+                <p class="ma-0">
+                  {{ element.card.description }}
+                </p>
+                <ul class="ma-0">
+                  <li v-for="(tool, j) in element.card.tools" :key="j"></li>
+                </ul>
+              </v-card-text>
+              <v-card-actions>
+                <!-- <nuxt-link :to="element.card.action">Read more</nuxt-link> -->
+                Read more
+              </v-card-actions>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -53,8 +80,10 @@ export default {
     return {
       timelineItems: [
         {
+          id: 1,
           avatar: {
-            img: "@/assets/images/conveos_logo.png",
+            img: "conveos_logo.png",
+            alt: "Logo of software agency Conveos",
             bgColor: "#000",
             label: { text: "Conveos", link: "https://conveos.com" },
           },
@@ -62,11 +91,28 @@ export default {
             role: "Back End Web Developer",
             period: { start: "Jan 2015", end: "March 2021" },
             description: "lorem ipsum",
+            tools: [],
           },
         },
         {
+          id: 2,
           avatar: {
-            img: "/assets/images/conveos_logo.png",
+            img: "army_logo.png",
+            bgColor: "blue",
+            label: { text: "Greek Army", link: "" },
+          },
+          card: {
+            role: "IT Technician",
+            period: { start: "Jan 2015", end: "March 2021" },
+            description: "",
+            tools: [],
+          },
+        },
+        {
+          id: 3,
+
+          avatar: {
+            img: "conveos_logo.png",
             bgColor: "#000",
             label: { text: "Conveos", link: "https://conveos.com" },
           },
@@ -74,12 +120,15 @@ export default {
             role: "Back End Web Developer",
             period: { start: "Jan 2015", end: "March 2021" },
             description: "lorem ipsum",
+            tools: [],
+
             action: "/job",
           },
         },
         {
+          id: 4,
           avatar: {
-            img: "@/assets/images/conveos_logo.png",
+            img: "conveos_logo.png",
             bgColor: "#000",
             label: { text: "Conveos", link: "https://conveos.com" },
           },
@@ -93,10 +142,31 @@ export default {
       ],
     };
   },
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    },
+  },
+  methods: {
+    navigateToCompany(cUrl) {
+      if (!cUrl || cUrl == "") return;
+      let url = cUrl;
+      window.open(url, "_blank");
+    },
+  },
 };
 </script>
 <style scoped>
 .firm-link {
   text-decoration: none;
+}
+
+.firm-logo {
+  width: 45px;
+  height: 45px;
+  cursor: pointer;
+}
+
+.timeline-item {
 }
 </style>
