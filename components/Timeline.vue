76ea022fe887785130b43468d2a07a3966bcbeda
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-07-06 08:23:52 +0300

    feat: populate timeline with data
    work experience

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -1,101 +1,102 @@
 <template>
-  <v-timeline :dense="$vuetify.breakpoint.smAndDown">
-    <v-timeline-item color="purple lighten-2" fill-dot right>
-      <v-card>
-        <v-card-title class="purple lighten-2">
-          <v-icon dark size="42" class="mr-4"> mdi-magnify </v-icon>
-          <h2 class="display-1 white--text font-weight-light">Title 1</h2>
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="10">
-              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
-              scaevola imperdiet nec ut, sed euismod convenire principes at. Est
-              et nobis iisque percipit.
-            </v-col>
-            <v-col class="hidden-sm-and-down text-right" md="2">
-              <v-icon size="64">mdi-calendar-text</v-icon>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-
-    <v-timeline-item color="amber lighten-1" fill-dot left small>
-      <v-card>
-        <v-card-title class="amber lighten-1 justify-end">
-          <h2 class="display-1 mr-4 white--text font-weight-light">Title 2</h2>
-          <v-icon dark size="42">mdi-home-outline</v-icon>
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="8">
-              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
-              scaevola imperdiet nec ut, sed euismod convenire principes at. Est
-              et nobis iisque percipit.
-            </v-col>
-            <v-col cols="12" md="4">
-              Lorem ipsum dolor sit amet, no nam oblique veritus.
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-
-    <v-timeline-item color="cyan lighten-1" fill-dot right>
-      <v-card>
-        <v-card-title class="cyan lighten-1">
-          <v-icon class="mr-4" dark size="42"> mdi-email-outline </v-icon>
-          <h2 class="display-1 white--text font-weight-light">Title 3</h2>
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col v-for="n in 3" :key="n" cols="12" md="4">
-              Lorem ipsum dolor sit amet, no nam oblique veritus no nam oblique.
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-
-    <v-timeline-item color="red lighten-1" fill-dot left small>
-      <v-card>
-        <v-card-title class="red lighten-1 justify-end">
-          <h2 class="display-1 mr-4 white--text font-weight-light">Title 4</h2>
-          <v-icon dark size="42"> mdi-account-multiple-outline </v-icon>
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col class="hidden-sm-and-down" md="2">
-              <v-icon size="64">mdi-server-network</v-icon>
-            </v-col>
-            <v-col cols="12" md="10">
-              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
-              scaevola imperdiet nec ut, sed euismod convenire principes at. Est
-              et nobis iisque percipit, an vim zril disputando voluptatibus.
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-
-    <v-timeline-item color="green lighten-1" fill-dot right>
-      <v-card>
-        <v-card-title class="green lighten-1">
-          <v-icon class="mr-4" dark size="42"> mdi-phone-in-talk </v-icon>
-          <h2 class="display-1 white--text font-weight-light">Title 5</h2>
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col>
-              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune
-              scaevola imperdiet nec ut, sed euismod convenire principes at. Est
-              et nobis iisque percipit, an vim zril disputando voluptatibus, vix
-              an salutandi sententiae.
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-timeline-item>
-  </v-timeline>
+  <v-container fluid>
+    <v-row align="center" class="justify-center justify-sm-left">
+      <v-col cols="auto">
+        <h3>Experience</h3>
+      </v-col>
+    </v-row>
+    <v-timeline>
+      <v-timeline-item v-for="(element, index) in timelineItems" :key="index">
+        <template #icon>
+          <v-avatar :color="element.avatar.bgColor">
+            <a
+              :href="element.avatar.label.link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img :src="element.avatar.img" />
+            </a>
+          </v-avatar>
+        </template>
+        <template #opposite>
+          <a
+            :href="element.avatar.label.link"
+            class="firm-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>{{ element.avatar.label.text }}</span>
+          </a>
+        </template>
+        <v-card class="elevation-2">
+          <v-card-title class="text-h6">
+            {{ element.card.role }}
+          </v-card-title>
+          <v-card-subtitle>
+            {{ element.card.period.start }} -
+            {{ element.card.period.end }}
+          </v-card-subtitle>
+          <v-card-text>{{ element.card.description }}</v-card-text>
+          <v-card-actions>
+            <!-- <nuxt-link :to="element.card.action">Read more</nuxt-link> -->
+            Read more
+          </v-card-actions>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      timelineItems: [
+        {
+          avatar: {
+            img: "@/assets/images/conveos_logo.png",
+            bgColor: "#000",
+            label: { text: "Conveos", link: "https://conveos.com" },
+          },
+          card: {
+            role: "Back End Web Developer",
+            period: { start: "Jan 2015", end: "March 2021" },
+            description: "lorem ipsum",
+          },
+        },
+        {
+          avatar: {
+            img: "/assets/images/conveos_logo.png",
+            bgColor: "#000",
+            label: { text: "Conveos", link: "https://conveos.com" },
+          },
+          card: {
+            role: "Back End Web Developer",
+            period: { start: "Jan 2015", end: "March 2021" },
+            description: "lorem ipsum",
+            action: "/job",
+          },
+        },
+        {
+          avatar: {
+            img: "@/assets/images/conveos_logo.png",
+            bgColor: "#000",
+            label: { text: "Conveos", link: "https://conveos.com" },
+          },
+          card: {
+            role: "Front End Web Developer",
+            period: { start: "Jan 2015", end: "March 2021" },
+            description: "lorem ipsum",
+            action: "/job",
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.firm-link {
+  text-decoration: none;
+}
+</style>
