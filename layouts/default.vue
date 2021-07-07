4ba658610fc5ac39df5613681825de221d30d0fe
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-07-07 09:43:15 +0300

    style: define font-families in global css file

--- layouts/default.vue
+++ layouts/default.vue
@@ -46,24 +46,7 @@ export default {
 };
 </script>
 <style>
-@font-face {
-  font-family: "Vision";
-  src: url("@/assets/fonts/Vision/WEB/Vision-Black.eot");
-  src: url("@/assets/fonts/Vision/WEB/Vision-Black.eot?#iefix")
-      format("embedded-opentype"),
-    url("../assets/fonts/Vision/WEB/Vision-Black.woff2") format("woff2"),
-    url("@/assets/fonts/Vision/WEB/Vision-Black.woff") format("woff"),
-    url("@/assets/fonts/Vision/TTF/Vision-Black.ttf") format("truetype");
-}
 
-@font-face {
-  font-family: "Manrope";
-  src: url("@/assets/fonts/Manrope/web/Manrope-Regular.woff2");
-  src: url("@/assets/fonts/Manrope/web/Manrope-Regular.woff") format("woff"),
-    url("@/assets/fonts/Manrope/otf/Manrope-Regular.otf")
-      format("embedded-opentype"),
-    url("@/assets/fonts/Manrope/ttf/Manrope-Regular.ttf") format("truetype");
-}
 
 .logo a {
   text-decoration: none;
