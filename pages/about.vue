4e992acbe5c97afab84bbd03044c593da4a8764c
gtopsis <csd3004@csd.uoc.gr>
2023-04-07 16:20:48 +0300

    Edit text of aboutMe section

--- pages/about.vue
+++ pages/about.vue
@@ -17,15 +17,17 @@ const head = {
   image: "./images/Twitter-card.png",
 };
 
+const webDevelopmentExperienceInYears = new Date().getFullYear() - 2017;
+
 const aboutMe = [
   `As a kid I was thinking that sooner or later I will find which is my dream job, in which direction I'm was born to shine having a unique and enviable talent. The mission was clear, to find my specialty!
 My studies (BSc, MSc, Erasmus periods) in Information Systems and Human-Computer Interaction at Computer Science Department of University of Crete seemed to offer me an appropriate opportunity to achieve my mission. And I was close, so close; Finally, I only had to choose between Web Development and User Experience Design fields.`,
   `
-Well, for more than 5 years (since 2016 when AngularJS was the king) I focus, utilizing the JavaScript ecosystem, on building software platforms for the web. The last 2 years I have worked both as a professional Front and Back End developer, transforming design mockups into usable pages and designing, developing and testing RESTful APIs and robust databases.`,
+Well, for more than ${webDevelopmentExperienceInYears} years (since 2016 when AngularJS was the king) I focus, utilizing the JavaScript ecosystem, on building software platforms for the web. The last 3 years I have worked both as a professional Front and Back End developer, transforming design mockups into usable pages and designing, developing and testing RESTful APIs as well as developing third-party intefrations utilizing several AWS services.`,
   `
-As a member of a team, I seek honest and trustworthy connections. In such environments, I feel secure, efficient and funny ツ. I'm not a fan of life hack "Fake it until you make it" and also I'm trying to be social without having presence on most social networking platforms.
+As a partner, I seek trustworthy connections and I enjoy being in work environments where meritocracy prevails. In such conditions, I feel a secure, productive, and funny teammate ツ.
 `,
-  `When I'm away from keyboard I like joining local meetups (DevStaff, Open Coffee Heraklion) as well as dance communities, learning and dancing mostly Argentinian Tango and Lindy Hop. Moreover, I love cycling around the seaside part of Heraklion and playing football with good friends in 8x8 fields weekly.`,
+  `When I'm away from keyboard I like joining local meetups (DevStaff, Open Coffee Heraklion) as well as dance communities, learning and dancing mostly Argentinian Tango and Lindy Hop.`,
 ];
 </script>
 
@@ -52,7 +54,7 @@ As a member of a team, I seek honest and trustworthy connections. In such enviro
       </v-col>
 
       <!-- about me section -->
-      <v-col cols="12" class="section px-4 py-10 mx-0">
+      <v-col cols="12" class="section px-0 py-10 mx-0">
         <v-container class="about-me-container" fluid full-width>
           <v-row full-width justify="center" align="center">
             <v-col cols="12" md="10" lg="9" class="px-4">
@@ -71,7 +73,7 @@ As a member of a team, I seek honest and trustworthy connections. In such enviro
       </v-col>
 
       <!-- timeline section -->
-      <v-col cols="12" class="section px-4 py-10 mx-0">
+      <v-col cols="12" class="section px-0 py-10 mx-0">
         <v-row class="justify-space-between no-gutters">
           <v-col cols="12" mb="2">
             <timeline content="experience"></timeline>
@@ -80,7 +82,7 @@ As a member of a team, I seek honest and trustworthy connections. In such enviro
       </v-col>
 
       <!-- timeline section -->
-      <v-col cols="12" class="section px-4 py-10 mx-0">
+      <v-col cols="12" class="section px-0 py-10 mx-0">
         <v-row class="justify-space-between no-gutters">
           <v-col cols="12" mb="2">
             <timeline content="education"></timeline>
